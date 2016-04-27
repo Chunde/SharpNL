4c966c266e062803ed8aaf93bf18f88660fb4025
Knuppe <gustavoknuppe@gmail.com>
2016-04-27 20:52:16 -0300

    Added a multi-threading test for the NameFinderME.

---
+++ src/SharpNL.Tests/CrossThreadTestRunner.cs
@@ -0,0 +1,76 @@
+﻿//  
+//  Copyright 2016 Gustavo J Knuppe (https://github.com/knuppe)
+//  
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//  
+//       http://www.apache.org/licenses/LICENSE-2.0
+//  
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//  
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   - May you do good and not evil.                                         -
+//   - May you find forgiveness for yourself and forgive others.             -
+//   - May you share freely, never taking more than you give.                -
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   
+
+using System;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace SharpNL.Tests {
+
+    /// <seealso href="http://blog.jerometerry.com/2014/06/multi-threaded-nunit-tests.html"/>
+    /// <remarks>
+    /// Created by: Jerome Terry 
+    /// </remarks>
+    internal class CrossThreadTestRunner {
+        private const string RemoteStackTraceFieldName = "_remoteStackTraceString";
+        private static readonly FieldInfo RemoteStackTraceField = typeof (Exception).GetField(RemoteStackTraceFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+        private readonly ThreadStart start;
+        private readonly Thread thread;
+        private Exception lastException;
+
+        public CrossThreadTestRunner(ThreadStart start) {
+            this.start = start;
+            thread = new Thread(Run);
+            thread.SetApartmentState(ApartmentState.STA);
+        }
+
+        public void Start() {
+            lastException = null;
+            thread.Start();
+        }
+
+        public void Join() {
+            thread.Join();
+
+            if (lastException != null) {
+                ThrowExceptionPreservingStack(lastException);
+            }
+        }
+
+        private void Run() {
+            try {
+                start.Invoke();
+            } catch (Exception e) {
+                lastException = e;
+            }
+        }
+
+        [ReflectionPermission(SecurityAction.Demand)]
+        private static void ThrowExceptionPreservingStack(Exception exception) {
+            if (RemoteStackTraceField != null) {
+                RemoteStackTraceField.SetValue(exception, exception.StackTrace + Environment.NewLine);
+            }
+            throw exception;
+        }
+    }
+}
---
+++ src/SharpNL.Tests/NameFind/NameFinderMultithreadingTest.cs
@@ -0,0 +1,86 @@
+﻿//  
+//  Copyright 2016 Gustavo J Knuppe (https://github.com/knuppe)
+//  
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//  
+//       http://www.apache.org/licenses/LICENSE-2.0
+//  
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//  
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   - May you do good and not evil.                                         -
+//   - May you find forgiveness for yourself and forgive others.             -
+//   - May you share freely, never taking more than you give.                -
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   
+
+
+using System;
+using NUnit.Framework;
+using SharpNL.NameFind;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SharpNL.Tokenize;
+
+namespace SharpNL.Tests.NameFind {
+
+    [TestFixture]
+    public class NameFinderMultithreadingTest {
+
+        internal const string fileName = "opennlp/models/en-ner-person.bin";
+
+        private TokenNameFinderModel modelFile;
+
+        [Test]
+        public void MultithreadingTest() {
+
+            const int threadCount = 100;
+
+            // The expensive part of the code is to load the model!
+            // but the model file can be shared.
+           
+            var fileStream = Tests.OpenFile(fileName); 
+            modelFile = new TokenNameFinderModel(fileStream);
+
+            var fileContents = File.ReadAllText(Tests.GetFullPath("/opennlp/tools/sentdetect/Sentences.txt"));
+            var sentences = fileContents.Split(new [] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            var rnd = new Random();
+            
+            var count = 0;
+
+            var delegates = new List<ThreadStart>(threadCount);
+            for (var i = 0; i < threadCount; i++)
+                delegates.Add(() => {
+
+                    // Use ONE NameFinderME instance per thread !
+
+                    var nameFinder = new NameFinderME(modelFile);
+                    var tokens = WhitespaceTokenizer.Instance.Tokenize(sentences[rnd.Next(0, sentences.Length - 1)]);
+
+                    Thread.Sleep(rnd.Next(100, 300));
+
+                    var names = nameFinder.Find(tokens);
+
+                    count += names.Length;
+                });
+            
+            var threads = delegates.Select(d => new CrossThreadTestRunner(d)).ToList();
+            foreach (var thread in threads)
+                thread.Start();
+
+            foreach (var thread in threads) 
+                thread.Join();
+
+            Assert.That(count, Is.GreaterThan(0));
+
+        }
+    }
+}
